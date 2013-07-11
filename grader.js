180469c29b27856a69a774d664f9eb1cce8c9c09
jprogweb <jprogweb@gmail.com>
2013-07-11 21:40:58 +0000

    HW3

--- grader.js
+++ grader.js
@@ -24,14 +24,16 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -49,8 +51,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -61,14 +63,28 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var descargarFichero = function (url) {
+    rest.get(url).on('success', function (data, response) {
+	fs.writeFileSync("temp.html", data);
+    });
+}
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url  <url>', 'URL to process')
+	.parse(process.argv);
+
+    var fichero = program.file;
+    if(program.url) {
+	descargarFichero(program.url);
+	fichero = 'temp.html';
+    }
+    var checkJson = checkHtmlFile(fichero, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
